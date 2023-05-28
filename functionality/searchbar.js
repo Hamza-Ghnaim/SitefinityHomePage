26dddf68202147e059567a00706bba437499d6ca
Hamza <hamzaghnaim99@gmail.com>
2023-05-28 16:43:57 -0700

    the navigation menu remains

--- functionality/searchbar.js
+++ functionality/searchbar.js
@@ -16,6 +16,18 @@
 // });
 
 window.onload = function() {
+  var mobileSearchIcon = document.getElementById("Mobilesearch-icon");
+  var mobileSearchInputContainer = document.querySelector(".MobilesearchInput-container");
+  
+  mobileSearchInputContainer.style.display = "none"; // Set initial state to hidden
+  
+  mobileSearchIcon.addEventListener("click", function() {
+    if (mobileSearchInputContainer.style.display === "none") {
+      mobileSearchInputContainer.style.display = "flex";
+    } else {
+      mobileSearchInputContainer.style.display = "none";
+    }
+  });
   var searchIcon = document.getElementById("search-icon");
   var searchInputContainer = document.getElementsByClassName("searchInput-container")[0];
   
--- search-toggle.js
+++ search-toggle.js
@@ -1,9 +1,14 @@
-const searchToggle = document.getElementById('search-toggle');
-const searchField = document.getElementById('search-field');
-const menuList = document.querySelector('.menu-list');
-
-searchToggle.addEventListener('click', () => {
-  if (!menuList.classList.contains('show')) {
-    searchField.classList.toggle('show');
-  }
-});
+// window.onload = function() {
+//   var mobileSearchIcon = document.getElementById("Mobilesearch-icon");
+//   var mobileSearchInputContainer = document.querySelector(".MobilesearchInput-container");
+  
+//   mobileSearchInputContainer.style.display = "none"; // Set initial state to hidden
+  
+//   mobileSearchIcon.addEventListener("click", function() {
+//     if (mobileSearchInputContainer.style.display === "none") {
+//       mobileSearchInputContainer.style.display = "block";
+//     } else {
+//       mobileSearchInputContainer.style.display = "none";
+//     }
+//   });
+// };
