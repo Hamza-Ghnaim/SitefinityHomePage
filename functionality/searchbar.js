011baf75c9ad99a73da79ba0b944c502eba0fc5c
Hamza <hamzaghnaim99@gmail.com>
2023-05-28 15:26:42 -0700

    searchbar for mobile

--- functionality/searchbar.js
+++ functionality/searchbar.js
@@ -4,29 +4,28 @@
 // searchIcon.addEventListener('click', function () {
 //   const inputVisibility = window.getComputedStyle(searchInput).getPropertyValue('visibility');
 //   searchInput.style.visibility = inputVisibility === 'hidden' ? 'visible' : 'hidden';
+//   searchInput.classList.toggle('visible');
 // });
 
 // window.addEventListener('click', function (event) {
 //   const clickedElement = event.target;
 //   if (clickedElement !== searchIcon && clickedElement !== searchInput) {
 //     searchInput.style.visibility = 'hidden';
+//     searchInput.classList.remove('visible');
 //   }
 // });
 
-
-const searchIcon = document.getElementById('search-icon');
-const searchInput = document.getElementById('search-input');
-
-searchIcon.addEventListener('click', function () {
-  const inputVisibility = window.getComputedStyle(searchInput).getPropertyValue('visibility');
-  searchInput.style.visibility = inputVisibility === 'hidden' ? 'visible' : 'hidden';
-  searchInput.classList.toggle('visible');
-});
-
-window.addEventListener('click', function (event) {
-  const clickedElement = event.target;
-  if (clickedElement !== searchIcon && clickedElement !== searchInput) {
-    searchInput.style.visibility = 'hidden';
-    searchInput.classList.remove('visible');
-  }
-});
+window.onload = function() {
+  var searchIcon = document.getElementById("search-icon");
+  var searchInputContainer = document.getElementsByClassName("searchInput-container")[0];
+  
+  searchInputContainer.style.display = "none"; // Set initial state to hidden
+  
+  searchIcon.addEventListener("click", function() {
+    if (searchInputContainer.style.display === "none") {
+      searchInputContainer.style.display = "flex";
+    } else {
+      searchInputContainer.style.display = "none";
+    }
+  });
+};
