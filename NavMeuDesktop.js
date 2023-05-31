fa27121cc80119fca836d6be7a4d5c54f391453c
Hamza <hamzaghnaim99@gmail.com>
2023-05-31 13:26:21 -0700

    done except the buttons part

--- NavMeuDesktop.js
+++ NavMeuDesktop.js
@@ -1,3 +1,31 @@
+// const capabilitiesLink = document.querySelector('.capabilities-link');
+// const capabilitiesMenu = document.querySelector('.capabilities-menu');
+// let isMenuOpen = false;
+
+// function updateMenuVisibility() {
+//   if (window.innerWidth < 768 || !isMenuOpen) {
+//     capabilitiesMenu.style.display = 'none';
+//   } else {
+//     capabilitiesMenu.style.display = 'block';
+//   }
+// }
+
+// capabilitiesLink.addEventListener('click', (event) => {
+//   event.preventDefault(); // Prevent the link from redirecting
+
+//   if (capabilitiesMenu.style.display === 'block') {
+//     capabilitiesMenu.style.display = 'none'; // Hide the capabilities menu if it's already visible
+//     isMenuOpen = false;
+//   } else {
+//     capabilitiesMenu.style.display = 'block'; // Show the capabilities menu
+//     isMenuOpen = true;
+//   }
+// });
+
+// window.addEventListener('resize', updateMenuVisibility); // Add event listener for resize
+
+// updateMenuVisibility(); // Call the function initially to set the initial menu visibility based on screen width
+  
 const capabilitiesLink = document.querySelector('.capabilities-link');
 const capabilitiesMenu = document.querySelector('.capabilities-menu');
 let isMenuOpen = false;
@@ -16,13 +44,14 @@ capabilitiesLink.addEventListener('click', (event) => {
   if (capabilitiesMenu.style.display === 'block') {
     capabilitiesMenu.style.display = 'none'; // Hide the capabilities menu if it's already visible
     isMenuOpen = false;
+    capabilitiesLink.style.color = ''; // Reset the link color
   } else {
     capabilitiesMenu.style.display = 'block'; // Show the capabilities menu
     isMenuOpen = true;
+    capabilitiesLink.style.color = '#DA6123'; // Set the link color to #DA6123
   }
 });
 
 window.addEventListener('resize', updateMenuVisibility); // Add event listener for resize
 
 updateMenuVisibility(); // Call the function initially to set the initial menu visibility based on screen width
-  
