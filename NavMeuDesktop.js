e902c0eda052df08686673df12388d1f109c366d
Hamza <hamzaghnaim99@gmail.com>
2023-05-30 18:34:02 -0700

    nvigation menu for desktop done

--- NavMeuDesktop.js
+++ NavMeuDesktop.js
@@ -1,21 +1,9 @@
-// const capabilitiesLink = document.querySelector('.capabilities-link');
-// const capabilitiesMenu = document.querySelector('.capabilities-menu');
-
-// capabilitiesLink.addEventListener('click', (event) => {
-//   event.preventDefault(); // Prevent the link from redirecting
-
-//   if (capabilitiesMenu.style.display === 'block') {
-//     capabilitiesMenu.style.display = 'none'; // Hide the capabilities menu if it's already visible
-//   } else {
-//     capabilitiesMenu.style.display = 'block'; // Show the capabilities menu if it's hidden
-//   }
-// });
-
 const capabilitiesLink = document.querySelector('.capabilities-link');
 const capabilitiesMenu = document.querySelector('.capabilities-menu');
+let isMenuOpen = false;
 
 function updateMenuVisibility() {
-  if (window.innerWidth < 768) {
+  if (window.innerWidth < 768 || !isMenuOpen) {
     capabilitiesMenu.style.display = 'none';
   } else {
     capabilitiesMenu.style.display = 'block';
@@ -27,11 +15,14 @@ capabilitiesLink.addEventListener('click', (event) => {
 
   if (capabilitiesMenu.style.display === 'block') {
     capabilitiesMenu.style.display = 'none'; // Hide the capabilities menu if it's already visible
+    isMenuOpen = false;
   } else {
-    capabilitiesMenu.style.display = 'block'; // Show the capabilities menu if it's hidden
+    capabilitiesMenu.style.display = 'block'; // Show the capabilities menu
+    isMenuOpen = true;
   }
 });
 
 window.addEventListener('resize', updateMenuVisibility); // Add event listener for resize
 
 updateMenuVisibility(); // Call the function initially to set the initial menu visibility based on screen width
+  
